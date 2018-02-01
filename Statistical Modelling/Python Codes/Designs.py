0981ea7b3062b2249325f8c2f2a8ff23d1d0e488
Taymour Khan Niazi <30389323+taymourniazi@users.noreply.github.com>
2018-02-01 12:49:01 +0500

    Update Designs.py

--- Statistical Modelling/Python Codes/Designs.py
+++ Statistical Modelling/Python Codes/Designs.py
@@ -1,9 +1,18 @@
 import numpy as np
 import pandas as pd
-import stats from scipy
+import scipy as sp
+from scipy import stats
 
-dataset=pd.read_xlsx('Designs.xlsx')
+dataset=pd.read_excel('Designs.xlsx')
 print(stats.describe(dataset['Con1']))
 
 #to include all number and string containing column
-dataset.describe(include = 'all')
+dataset.describe(include ='all')
+
+#to calculate variance
+'''ddof delta degree of freedom, a parameter set to one
+so it wont differ'''
+np.var(dataset['Con1'], ddof=1)
+
+#to calculate variance
+np.std(dataset['Con1'],ddof=1)
