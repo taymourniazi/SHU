29d2b98b9d500e361911f4f05af7843f23f4361a
Taymour Khan Niazi <30389323+taymourniazi@users.noreply.github.com>
2018-02-01 14:26:53 +0500

    Update Designs.py

--- Statistical Modelling/Python Codes/Designs.py
+++ Statistical Modelling/Python Codes/Designs.py
@@ -2,21 +2,21 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn.apionly as sns
 
 dataset=pd.read_excel('Designs.xlsx')
-print(stats.describe(dataset['Con1']))
-
+print(stats.describe(dataset[['Con1','Con2']]))
 #to include all number and string containing column
 dataset.describe(include ='all')
-
 #to calculate variance
 '''ddof delta degree of freedom, a parameter set to one
 so it wont differ'''
-np.var(dataset['Con1'], ddof=1)
+np.var(dataset[['Con1','Con2']], ddof=1)
 
 #to calculate std
-np.std(dataset['Con1'],ddof=1)
+np.std(dataset[['Con1','Con2']],ddof=1)
 #best way to do std dev
-dataset['Con1'].std()
+dataset[['Con1','Con2']].std()
 #just extra (to calculate mode)
-stats.mode(dataset['Con1'])
+stats.mode(dataset[['Con1','Con2']]
